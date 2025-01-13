d07b219534e3cc3f2fd175469240d87c6320d0c9
Theo Archer <117527935+Kalibercoder@users.noreply.github.com>
2025-01-13 11:02:05 +0100

    added pages

---
+++ src/components/navigation.js
@@ -0,0 +1,21 @@
+// filepath: /Users/theostenman/Documents/GitHub/workspace/headless/portfolio/src/components/navigation.js
+import * as React from "react"
+import { Link } from "gatsby"
+
+const Navigation = () => (
+  <nav>
+    <ul style={{ display: 'flex', listStyle: 'none', justifyContent: 'space-around', padding: 20 }}>
+      <li><Link to="/homepage" style={{ color: 'white' }}>Home</Link></li>
+      -
+      <li><Link to="/about" style={{ color: 'white' }}>About</Link></li>
+      -
+      <li><Link to="/contact" style={{ color: 'white' }}>Contact</Link></li>
+      -
+      <li><Link to="/heroproject" style={{ color: 'white' }}>Heroproject</Link></li>
+      -
+      <li><Link to="/projects" style={{ color: 'white' }}>Projects</Link></li>
+    </ul>
+  </nav>
+)
+
+export default Navigation
---
+++ src/pages/heroproject.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Navigation from "../components/navigation"
+
+const IndexPage = () => {
+    return (
+      <main style={{ backgroundColor: 'black', margin: '25vh', height: '50vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Navigation />
+        <h1 style={{ color: 'white' }}>heroproject</h1>
+      </main>
+    )
+  }
+  
+  export default IndexPage
